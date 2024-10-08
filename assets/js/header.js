31b3601216b79f033c0cfee891ae94f5d9fdc52d
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2024-10-08 14:41:12 +0500

    swiper start

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,15 +1,13 @@
-const CONTENT_TOGGLER = document.querySelector('#main-body-content-toggler')
-const MAIN_BODY_CONTENT = document.querySelector('#main-body-content')
-const leftMenu = document.querySelector('.left-menu')
-const mainContent = document.querySelector('.main-content')
+const CONTENT_TOGGLER = document.querySelector("#main-body-content-toggler");
+const MAIN_BODY_CONTENT = document.querySelector("#main-body-content");
+const leftMenu = document.querySelector(".left-menu");
+const mainContent = document.querySelector(".main-content");
 
+console.log(CONTENT_TOGGLER);
 
-console.log(CONTENT_TOGGLER)
+CONTENT_TOGGLER.addEventListener("click", (e) => {
+  console.log("clicked");
+  leftMenu.classList.toggle("show-left-menu");
+  mainContent.classList.toggle("show-left-menu");
+});
 
-CONTENT_TOGGLER.addEventListener('click', (e) => {
-    console.log("clicked")
-    leftMenu.classList.toggle('show-left-menu');
-    mainContent.classList.toggle('show-left-menu')
-})
-
-// 
