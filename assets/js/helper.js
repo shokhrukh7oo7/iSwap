8f3537f5cf1e4190b35fb0507ea37a6ad1059a76
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2024-10-08 18:34:43 +0500

    product evalution section fixed

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 10,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      450: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
   });
+});
