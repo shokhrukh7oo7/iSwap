742a29b86f846282f9db95c0c623b36d30025678
Alisher <alisherxujanov163@gmail.com>
2024-10-09 10:10:12 +0500

    update

--- locale/translations.js
+++ locale/translations.js
@@ -4,18 +4,21 @@ const translations = {
         "lang_name": "English",
         "hello": "Hello {name}",
         "viloyati": "Region",
+        "toshkent": "Tashkent",
     },
     "ru": {
         "lang_code": "ru",
         "lang_name": "Русский",
         "hello": "Привет {name}",
         "viloyati": "Регион",
+        "toshkent": "Ташкент",
     },
     "uz": {
         "lang_code": "uz",
         "lang_name": "Uzbek",
         "hello": "Salom {name}",
         "viloyati": "Viloyat",
+        "toshkent": "Toshkent",
     }
 }
 export default translations
